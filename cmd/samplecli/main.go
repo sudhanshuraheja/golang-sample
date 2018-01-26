a279cef9652c07d2d8708274c45e14959eb00bf6
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-27 06:29:05 +0700

    Added a command line interface

--- cmd/samplecli/main.go
+++ cmd/samplecli/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+
+	"github.com/urfave/cli"
+
 	"github.com/sudhanshuraheja/golang-sample/pkg/config"
 	"github.com/sudhanshuraheja/golang-sample/pkg/logger"
 )
@@ -10,4 +14,43 @@ func main() {
 	logger.Init()
 
 	logger.Infoln("Sample CLI")
+	Init()
+}
+
+// Init : start the cli wrapper
+func Init() *cli.App {
+	app := cli.NewApp()
+	app.Name = config.Name()
+	app.Version = config.Version()
+	app.Usage = "this service is a sample golang service"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "start",
+			Usage: "start the service",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "migrate",
+			Usage: "run database migrations",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:  "rollback",
+			Usage: "rollback the latest database migration",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+
+	return app
 }
