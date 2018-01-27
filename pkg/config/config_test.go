5da6955ac5131fc1a06359b43d197f3970740b72
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-28 02:12:33 +0700

    Fixed test for config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -10,6 +10,6 @@ import (
 func TestConfigValues(t *testing.T) {
 	conf := config.NewConfig()
 	assert.Equal(t, "debug", conf.LogLevel())
-	assert.Equal(t, "dbname=sample user=sample password='sample' host=postgres port=5432 sslmode=disable", conf.Database().ConnectionString())
-	assert.Equal(t, "postgres://sample:sample@postgres:5432/sample?sslmode=disable", conf.Database().ConnectionURL())
+	assert.Equal(t, "dbname=sample user=sample password='sample' host=localhost port=5432 sslmode=disable", conf.Database().ConnectionString())
+	assert.Equal(t, "postgres://sample:sample@localhost:5432/sample?sslmode=disable", conf.Database().ConnectionURL())
 }
