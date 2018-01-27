4869b18cd6a3474fe6c2b952478dd15f2a4258e2
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-28 01:01:29 +0700

    Moved logger away from a singleton, added to context

--- pkg/appcontext/appcontext.go
+++ pkg/appcontext/appcontext.go
@@ -2,18 +2,25 @@ package appcontext
 
 import (
 	"github.com/sudhanshuraheja/golang-sample/pkg/config"
+	"github.com/sudhanshuraheja/golang-sample/pkg/logger"
 )
 
 type AppContext struct {
 	config *config.Config
+	logger *logger.Logger
 }
 
-func New(config *config.Config) *AppContext {
+func NewAppContext(config *config.Config, logger *logger.Logger) *AppContext {
 	return &AppContext{
 		config: config,
+		logger: logger,
 	}
 }
 
 func (a *AppContext) GetConfig() *config.Config {
 	return a.config
+}
+
+func (a *AppContext) GetLogger() *logger.Logger {
+	return a.logger
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,113 +2,28 @@ package logger
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/sudhanshuraheja/golang-sample/pkg/config"
 )
 
-var logger *logrus.Logger
+type Logger struct {
+	*logrus.Logger
+}
 
-func Init() {
+func NewLogger(config *config.Config) *Logger {
 	level, err := logrus.ParseLevel(config.LogLevel())
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	logger = &logrus.Logger{
-		Out:       os.Stdout,
-		Hooks:     make(logrus.LevelHooks),
-		Level:     level,
-		Formatter: &logrus.TextFormatter{},
+	return &Logger{
+		&logrus.Logger{
+			Out:       os.Stdout,
+			Hooks:     make(logrus.LevelHooks),
+			Level:     level,
+			Formatter: &logrus.TextFormatter{},
+		},
 	}
-}
-
-func AddHook(hook logrus.Hook) {
-	logger.Hooks.Add(hook)
-}
-
-func Debug(args ...interface{}) {
-	logger.Debug(args...)
-}
-
-func Debugf(format string, args ...interface{}) {
-	logger.Debugf(format, args...)
-}
-
-func Debugln(args ...interface{}) {
-	logger.Debugln(args...)
-}
-
-func Error(args ...interface{}) {
-	logger.Error(args...)
-}
-
-func Errorf(format string, args ...interface{}) {
-	logger.Errorf(format, args...)
-}
-
-func Errorln(args ...interface{}) {
-	logger.Errorln(args...)
-}
-
-func Errorrf(r *http.Request, format string, args ...interface{}) {
-	httpRequestLogEntry(r).Errorf(format, args...)
-}
-
-func Info(args ...interface{}) {
-	logger.Info(args...)
-}
-
-func Infof(format string, args ...interface{}) {
-	logger.Infof(format, args...)
-}
-
-func Infoln(args ...interface{}) {
-	logger.Infoln(args...)
-}
-
-func Inforf(r *http.Request, format string, args ...interface{}) {
-	httpRequestLogEntry(r).Infof(format, args...)
-}
-
-func Warn(args ...interface{}) {
-	logger.Warn(args...)
-}
-
-func Warnf(format string, args ...interface{}) {
-	logger.Warnf(format, args...)
-}
-
-func Warnln(args ...interface{}) {
-	logger.Warnln(args...)
-}
-
-func Fatal(args ...interface{}) {
-	logger.Fatal(args...)
-}
-
-func Fatalf(format string, args ...interface{}) {
-	logger.Fatalf(format, args...)
-}
-
-func Fatalln(args ...interface{}) {
-	logger.Fatalln(args...)
-}
-
-func WithField(key string, value interface{}) *logrus.Entry {
-	return logger.WithField(key, value)
-}
-
-func WithFields(fields logrus.Fields) *logrus.Entry {
-	return logger.WithFields(fields)
-}
-
-func httpRequestLogEntry(r *http.Request) *logrus.Entry {
-	return logger.WithFields(logrus.Fields{
-		"RequestMethod": r.Method,
-		"Host":          r.Host,
-		"Path":          r.URL.Path,
-	})
 }
